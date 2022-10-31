b71566e384af4a20f2dfa018f3c1bb49e0956a23
Eric Coleman <eric.coleman@frontierbpm.com>
2022-10-31 18:08:33 -0400

    commented out server connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,14 @@
-const mysql = require("mysql2");
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "surveyjs-demo",
-});
+// const mysql = require("mysql2");
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "surveyjs-demo",
+// });
 
-db.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected to database!");
-});
+// db.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected to database!");
+// });
 
-module.export = db;
+// module.export = db;

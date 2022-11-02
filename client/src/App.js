98830af369be52ed0c6664656bd2a3c0199534f6
Eric Coleman <eric.coleman@frontierbpm.com>
2022-11-02 18:15:40 -0400

    Added survey creator

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,44 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import SurveyComponent from "./components/SurveyComponent.jsx";
+import SurveyComponent from "./components/SurveyComponent.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<SurveyComponent />, document.getElementById("surveyElement"));
+import "./App.css";
+import { SurveyCreatorWidget } from "./components/SurveyCreator";
+
+function App() {
+  //   var surveyBtn = () => {
+  //     return (
+  //       <div>
+  //         <SurveyComponent></SurveyComponent>
+  //       </div>
+  //     );
+  //   };
+  //   var creatorBtn = () => {
+  //     return <SurveyCreatorWidget></SurveyCreatorWidget>;
+  //   };
+  return (
+    // <SurveyComponent></SurveyComponent>
+    <SurveyCreatorWidget></SurveyCreatorWidget>
+    // <div className="App">
+    //   <button onClick={surveyBtn()}>Take a survey</button>
+    //   {/* <SurveyComponent showCompletedPage={false}></SurveyComponent> */}
+    //   <br></br>
+    //   <button onClick={creatorBtn()}>Create a Survey</button>
+    //   {/* <SurveyCreatorWidget></SurveyCreatorWidget> */}
+    // // </div>
+    // <Router>
+    //   <Routes>
+    //     <Route exact path="/survey" component={SurveyComponent()}></Route>
+    //     <Route
+    //       exact
+    //       path="/surveyCreator"
+    //       component={SurveyCreatorWidget()}
+    //     ></Route>
+    //     {/* Catch all route */}
+    //     <Route component={SurveyComponent()}></Route>
+    //   </Routes>
+    // </Router>
+  );
+}
+
+export default App;
